2f229be589939dbdceb8495fa6cecde61889a938
Adam Gamble <adamgamble@gmail.com>
2011-01-20 11:13:51 -0600

    added week before and week after methods

--- lib/date.rb
+++ lib/date.rb
@@ -2,4 +2,12 @@ class Date
   def week
     Week.of_day self
   end
+
+  def week_before
+    Week.of_day self.beginning_of_week.advance(:days => -1)
+  end
+
+  def week_after
+    Week.of_day self.end_of_week.advance(:days => 1)
+  end
 end
